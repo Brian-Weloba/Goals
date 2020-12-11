bcff31dcea37a4671b46d8f49358c037fcfe012f
Brian <bweloba@gmail.com>
2020-12-11 16:22:35 +0300

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { GoalComponent } from './goal/goal.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'goals', component: GoalComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'goals/:id', component: GoalDetailComponent },
+  { path: '', redirectTo: "/goals", pathMatch: "full" },
+  { path: '**', component: NotFoundComponent },
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
